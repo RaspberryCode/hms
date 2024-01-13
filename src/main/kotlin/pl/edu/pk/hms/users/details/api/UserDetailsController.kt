215c76432f08e40a4df3794f36df275c8aa25005
RaspberryCode <tmalinowski@softwarekitchen.pl>
2024-01-13 01:09:28 +0100

    Update UserDetailsController documentation,

--- src/main/kotlin/pl/edu/pk/hms/users/details/api/UserDetailsController.kt
+++ src/main/kotlin/pl/edu/pk/hms/users/details/api/UserDetailsController.kt
@@ -1,5 +1,7 @@
 package pl.edu.pk.hms.users.details.api
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import pl.edu.pk.hms.users.User
+import pl.edu.pk.hms.users.authentiation.annotations.IsUser
 import pl.edu.pk.hms.users.details.UserDetailsPatchService
 import pl.edu.pk.hms.users.details.api.dto.UserDetailsPatchRequest
 import pl.edu.pk.hms.users.details.api.dto.UserDetailsResponse
@@ -20,6 +23,15 @@ import pl.edu.pk.hms.users.details.api.dto.UserDetailsResponse
 class UserDetailsController(private val userDetailsPatchService: UserDetailsPatchService) {
 
     @GetMapping
+    @IsUser
+    @Operation(
+        summary = "Get user details",
+        description = "Returns user details",
+        responses = [
+            ApiResponse(responseCode = "200", description = "User details found"),
+            ApiResponse(responseCode = "401", description = "Only allowed for authenticated users")
+        ]
+    )
     fun getDetails(authentication: Authentication): ResponseEntity<UserDetailsResponse> {
         val user = authentication.principal as User
         val userDetails = userDetailsPatchService.getUserDetails(user.id!!)
@@ -28,6 +40,15 @@ class UserDetailsController(private val userDetailsPatchService: UserDetailsPatc
     }
 
     @PatchMapping
+    @IsUser
+    @Operation(
+        summary = "Update user details",
+        description = "Updates user details",
+        responses = [
+            ApiResponse(responseCode = "200", description = "User details updated"),
+            ApiResponse(responseCode = "401", description = "Only allowed for authenticated users")
+        ]
+    )
     fun updateDetails(
         @Valid @RequestBody request: UserDetailsPatchRequest,
         authentication: Authentication
